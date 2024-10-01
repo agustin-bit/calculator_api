475b73898c43b1d00f2aaee93bfaad03f56c77b8
Agustín Peruzzi <agustin.peruzzi.latinad@gmail.com>
2024-10-01 20:46:43 -0300

    Prettify index

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
+const {
+  inchToCm,
+  cmToInch,
+  celciusToFahrenheit,
+  fahrenheitToCelsius,
+  poundsToKgs,
+  kgsToPounds,
+} = require("./converters.js");
 
-const { inchToCm, cmToInch, celciusToFahrenheit, fahrenheitToCelsius, poundsToKgs, kgsToPounds } = require('./converters.js');
-
-const app = express()
-app.use(cors({
-    origin: '*', // Replace with your frontend's URL
-}));
-
+const app = express();
+app.use(
+  cors({
+    origin: "*", // Replace with your frontend's URL
+  })
+);
 
 const convertUnits = (rawValue, conversor) => {
-    const value = parseFloat(rawValue);
-    const result = conversor(value);
-    return { originalValue: value, result };
-}
+  const value = parseFloat(rawValue);
+  const result = conversor(value);
+  return { originalValue: value, result };
+};
 
-app.get('/inch_to_cm/:inch', (req, res) => {
-    res.send(convertUnits(req.params.inch, inchToCm));
+app.get("/inch_to_cm/:inch", (req, res) => {
+  res.send(convertUnits(req.params.inch, inchToCm));
 });
 
-app.get('/cm_to_inch/:inch', (req, res) => {
-    res.send(convertUnits(req.params.inch, cmToInch));
+app.get("/cm_to_inch/:inch", (req, res) => {
+  res.send(convertUnits(req.params.inch, cmToInch));
 });
 
-app.get('/fahrenheit_to_celsius/:fahrenheit', (req, res) => {
-    res.send(convertUnits(req.params.fahrenheit, fahrenheitToCelsius));
+app.get("/fahrenheit_to_celsius/:fahrenheit", (req, res) => {
+  res.send(convertUnits(req.params.fahrenheit, fahrenheitToCelsius));
 });
 
-app.get('/celsius_to_fahrenheit/:celsius', (req, res) => {
-    res.send(convertUnits(req.params.celsius, celciusToFahrenheit));
+app.get("/celsius_to_fahrenheit/:celsius", (req, res) => {
+  res.send(convertUnits(req.params.celsius, celciusToFahrenheit));
 });
 
-app.get('/pounds_to_kgs/:pounds', (req, res) => {
-    res.send(convertUnits(req.params.pounds, poundsToKgs));
+app.get("/pounds_to_kgs/:pounds", (req, res) => {
+  res.send(convertUnits(req.params.pounds, poundsToKgs));
 });
 
-app.get('/kgs_to_pounds/:kgs', (req, res) => {
-    res.send(convertUnits(req.params.kgs, kgsToPounds));
+app.get("/kgs_to_pounds/:kgs", (req, res) => {
+  res.send(convertUnits(req.params.kgs, kgsToPounds));
 });
-
 
 app.listen(3000, () => {
-    console.log('API Started. Listening on port 3000')
-})
+  console.log("API Started. Listening on port 3000");
+});
